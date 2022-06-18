851164d0920ff79408df9a953d9c974058f82871
Dolzodmaa Davaasuren <dolzodmaadavaasuren@Dolzodmaas-MacBook-Pro.local>
2022-06-18 15:31:15 -0700

    metrics

--- dataset.py
+++ dataset.py
@@ -1,93 +1,47 @@
 
 import tensorflow as tf
-import keras
 import numpy as np
 import os
-import segmentation_models_3D as sm
 from skimage import io, img_as_float, img_as_ubyte
 from patchify import patchify, unpatchify
 import numpy as np
 from matplotlib import pyplot as plt
 from keras import backend as K
-from tensorflow.keras.layers import Input, Conv3D,Conv2D, SpatialDropout3D,SpatialDropout2D, Conv3DTranspose, Conv2DTranspose,MaxPooling3D,MaxPooling2D, concatenate
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.models import Model
 from sklearn.model_selection import train_test_split
 import cv2
 from skimage.transform import rescale, resize
 from PIL import Image
 from tifffile import imsave, imread
-from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, LearningRateScheduler
 from skimage.restoration import denoise_nl_means, estimate_sigma
-#tensorflow.from tensorflow.python.keras.utils import multi_gpu_model
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import glob
+from utils import resize_to_512, make_patches
 
 def dataset_loader(img_dir, mask_dir):
 
-
-##Convert grey image to 3 channels by copying channel 3 times.
-#We do this as our unet model expects 3 channel input. 
-
-    train_img_dir = os.listdir(img_dir_slice)
-    train_mask_dir = os.listdir(label_dir_slice)
-    name = {'13.tif':'0.tif', '14.tif':'1.tif', '7.tif':'2.tif', '8.tif':'3.tif', 'N-1.tif':'4.tif', 'N-11.tif':'5.tif',
-            'N-2.tif':'6.tif', 'N-5.tif':'7.tif', 'N-9.tif':'8.tif'}
+    train_img_dir = os.listdir(img_dir)
+    train_mask_dir = os.listdir(mask_dir)
 
     input_img, input_mask = [], []
     for i, img in enumerate(sorted(train_img_dir)):
-    print(img)
-    image = imread(img_dir_slice+img)
-    input_img.append(image)
-    #image = resize_to_512(image)
-    #sample = expand_dims(image, 0)
-    #it = datagen.flow(sample, batch_size=1)
-    #image = repeat_to_512(image, image.shape[0], 32)
-    #print(image.shape)
-    #print(image.max())
-
-    #img_patches = make_patches(image) 
-    #print(img_patches.shape)
-    """if i==0:
-        input_img = img_patches
-    else:
-        input_img = np.concatenate((input_img, img_patches), axis = 0)
-    aug_img = io.imread(img_dir+'0'+img)
-    aug_img = resize_to_512(aug_img)
-    aug_img = aug_img[:, 100:356, 100:356]
-    aug_img /= aug_img.max()
-    aug_img_patches = make_patches(aug_img)
-    input_img = np.concatenate((input_img, aug_img_patches), axis = 0)"""
-
-    mask = io.imread(label_dir_slice+img)
-    input_mask.append(mask)
-    #mask = resize_to_512(mask)
-    #mask = resize_to_512(mask)
-    #mask = pad_to_full(mask, mask.shape[0])
-    #mask_patches = make_patches(mask)
-    """if i==0:
-        input_mask = mask_patches
-    else:
-        input_mask = np.concatenate((input_mask, mask_patches), axis = 0) 
-    aug_mask = io.imread(label_dir+'0'+'label-'+img)
-    aug_mask = resize_to_512(aug_mask)
-    aug_mask = aug_mask[:, 100:356, 100:356]
-    aug_mask /= aug_mask.max()
-    aug_mask_patches = make_patches(aug_mask)
-    input_mask = np.concatenate((input_mask, aug_mask_patches), axis = 0)
-    for i in range(1):
-        gen_img = np.squeeze(io.imread(img_dir+'g'+str(i)+'_img'+ name[img]), axis=0)
-        print(gen_img.shape)
-        gen_img = repeat_to_512(gen_img, gen_img.shape[0], 32)
-        gen_img_patches = make_patches(gen_img)
-        input_img = np.concatenate((input_img, gen_img_patches), axis = 0)
-
-        gen_mask = np.squeeze(io.imread(label_dir+'g'+str(i)+'_label'+ name[img]), axis=0)
-        gen_mask = repeat_to_512(gen_mask, gen_mask.shape[0], 32)
-        gen_mask_patches = make_patches(gen_mask)
-        input_mask = np.concatenate((input_mask, gen_mask_patches), axis = 0)
-    """
+   
+        image = imread(img_dir+img)
+        image = resize_to_512(image)
+        input_img.append(image)
+        img_patches = make_patches(image) 
+        print(img_patches.shape)
+        if i==0:
+            input_img = img_patches
+        else:
+            input_img = np.concatenate((input_img, img_patches), axis = 0)
+    
+        mask = io.imread(mask_dir+img)
+        input_mask.append(mask)
+        mask = resize_to_512(mask)
+        mask_patches = make_patches(mask)
+        if i==0:
+            input_mask = mask_patches
+        else:
+            input_mask = np.concatenate((input_mask, mask_patches), axis = 0) 
+    
 
     input_img = np.array(input_img).astype(np.float32)
     input_mask = np.array(input_mask).astype(np.float32)
--- metrics.py
+++ metrics.py
@@ -5,27 +5,6 @@ SMOOTH = 1e-5
 
 
 class IOUScore(Metric):
-    r""" The `Jaccard index`_, also known as Intersection over Union and the Jaccard similarity coefficient
-    (originally coined coefficient de communauté by Paul Jaccard), is a statistic used for comparing the
-    similarity and diversity of sample sets. The Jaccard coefficient measures similarity between finite sample sets,
-    and is defined as the size of the intersection divided by the size of the union of the sample sets:
-    .. math:: J(A, B) = \frac{A \cap B}{A \cup B}
-    Args:
-        class_weights: 1. or ``np.array`` of class weights (``len(weights) = num_classes``).
-        class_indexes: Optional integer or list of integers, classes to consider, if ``None`` all classes are used.
-        smooth: value to avoid division by zero
-        per_image: if ``True``, metric is calculated as mean over images in batch (B),
-            else over whole batch
-        threshold: value to round predictions (use ``>`` comparison), if ``None`` prediction will not be round
-    Returns:
-       A callable ``iou_score`` instance. Can be used in ``model.compile(...)`` function.
-    .. _`Jaccard index`: https://en.wikipedia.org/wiki/Jaccard_index
-    Example:
-    .. code:: python
-        metric = IOUScore()
-        model.compile('SGD', loss=loss, metrics=[metric])
-    """
-
     def __init__(
             self,
             class_weights=None,
@@ -57,34 +36,6 @@ class IOUScore(Metric):
 
 
 class FScore(Metric):
-    r"""The F-score (Dice coefficient) can be interpreted as a weighted average of the precision and recall,
-    where an F-score reaches its best value at 1 and worst score at 0.
-    The relative contribution of ``precision`` and ``recall`` to the F1-score are equal.
-    The formula for the F score is:
-    .. math:: F_\beta(precision, recall) = (1 + \beta^2) \frac{precision \cdot recall}
-        {\beta^2 \cdot precision + recall}
-    The formula in terms of *Type I* and *Type II* errors:
-    .. math:: L(tp, fp, fn) = \frac{(1 + \beta^2) \cdot tp} {(1 + \beta^2) \cdot fp + \beta^2 \cdot fn + fp}
-    where:
-         - tp - true positives;
-         - fp - false positives;
-         - fn - false negatives;
-    Args:
-        beta: Integer of float f-score coefficient to balance precision and recall.
-        class_weights: 1. or ``np.array`` of class weights (``len(weights) = num_classes``)
-        class_indexes: Optional integer or list of integers, classes to consider, if ``None`` all classes are used.
-        smooth: Float value to avoid division by zero.
-        per_image: If ``True``, metric is calculated as mean over images in batch (B),
-            else over whole batch.
-        threshold: Float value to round predictions (use ``>`` comparison), if ``None`` prediction will not be round.
-        name: Optional string, if ``None`` default ``f{beta}-score`` name is used.
-    Returns:
-        A callable ``f_score`` instance. Can be used in ``model.compile(...)`` function.
-    Example:
-    .. code:: python
-        metric = FScore()
-        model.compile('SGD', loss=loss, metrics=[metric])
-    """
 
     def __init__(
             self,
@@ -120,27 +71,6 @@ class FScore(Metric):
 
 
 class Precision(Metric):
-    r"""Creates a criterion that measures the Precision between the
-    ground truth (gt) and the prediction (pr).
-    .. math:: F_\beta(tp, fp) = \frac{tp} {(tp + fp)}
-    where:
-         - tp - true positives;
-         - fp - false positives;
-    Args:
-        class_weights: 1. or ``np.array`` of class weights (``len(weights) = num_classes``).
-        class_indexes: Optional integer or list of integers, classes to consider, if ``None`` all classes are used.
-        smooth: Float value to avoid division by zero.
-        per_image: If ``True``, metric is calculated as mean over images in batch (B),
-            else over whole batch.
-        threshold: Float value to round predictions (use ``>`` comparison), if ``None`` prediction will not be round.
-        name: Optional string, if ``None`` default ``precision`` name is used.
-    Returns:
-        A callable ``precision`` instance. Can be used in ``model.compile(...)`` function.
-    Example:
-    .. code:: python
-        metric = Precision()
-        model.compile('SGD', loss=loss, metrics=[metric])
-    """
 
     def __init__(
             self,
@@ -173,27 +103,6 @@ class Precision(Metric):
 
 
 class Recall(Metric):
-    r"""Creates a criterion that measures the Precision between the
-    ground truth (gt) and the prediction (pr).
-    .. math:: F_\beta(tp, fn) = \frac{tp} {(tp + fn)}
-    where:
-         - tp - true positives;
-         - fn - false negatives;
-    Args:
-        class_weights: 1. or ``np.array`` of class weights (``len(weights) = num_classes``).
-        class_indexes: Optional integer or list of integers, classes to consider, if ``None`` all classes are used.
-        smooth: Float value to avoid division by zero.
-        per_image: If ``True``, metric is calculated as mean over images in batch (B),
-            else over whole batch.
-        threshold: Float value to round predictions (use ``>`` comparison), if ``None`` prediction will not be round.
-        name: Optional string, if ``None`` default ``recall`` name is used.
-    Returns:
-        A callable ``recall`` instance. Can be used in ``model.compile(...)`` function.
-    Example:
-    .. code:: python
-        metric = Precision()
-        model.compile('SGD', loss=loss, metrics=[metric])
-    """
 
     def __init__(
             self,

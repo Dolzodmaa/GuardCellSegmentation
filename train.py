93f3c66ae101851d9258eef2328c12bfbc31e980
Dolzodmaa Davaasuren <dolzodmaadavaasuren@Dolzodmaas-MacBook-Pro.local>
2022-06-18 15:33:40 -0700

    train added

--- train.py
+++ train.py
@@ -19,7 +19,9 @@ from skimage.restoration import denoise_nl_means, estimate_sigma
 #tensorflow.from tensorflow.python.keras.utils import multi_gpu_model
 from tensorflow.keras.models import load_model
 from loss import dice_loss, binary_focal_loss
-
+from model import Attention_UNet
+from dataset import dataset_loader
+from metrics import iou_score, f1_score, precision, recall
 
 def get_args():
     parser = argparse.ArgumentParser(
@@ -27,53 +29,38 @@ def get_args():
     )
    
     parser.add_argument("--log_dir", help="directory for logging.")
+    parser.add_argument("--img_dir", help="training image location")
+    parser.add_argument("--mask_dir", help="training label location")
     parser.add_argument("--learning_rate", default = 0.001)
     parser.add_argument("--epoch", default = 100, help="number of epochs to train")
-    parser.add_argument("--")
-
-    
+    parser.add_argument("--batch_size", default = 4, help="batch size for training")
+    parser.add_argument("--gamma_total_loss", default = 1, help="weight for focal loss")
+    parser.add_argument("--dropout_rate", default = 0.15, help="dropout rate")
     args = parser.parse_args()
     return args
-
-
-#Define parameters for our model.
-
-
-
 
 def main():
     args = get_args()
    
-    encoder_weights = 'imagenet'
-    BACKBONE = 'densenet121' 
+    BACKBONE = 'densenet' 
     activation = 'sigmoid'
-    patch_size = 4
-    n_classes = 1
-    channels=3
-
-    LR = 0.001
+    LR = args.learning_rate
     optim = tf.keras.optimizers.Adam(LR)
-    gamma = 1
+    gamma = args.gamma_total_loss
     total_loss = dice_loss + (gamma * binary_focal_loss)
+    metrics = [iou_score, f1_score, precision, recall]
 
-    # actulally total_loss can be imported directly from library, above example just show you how to manipulate with losses
-    # total_loss = sm.losses.binary_focal_dice_loss # or sm.losses.categorical_focal_dice_loss 
-
-    metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
-
-    model = sm.AttnUnet(BACKBONE, classes=n_classes, 
+    model = Attention_UNet(BACKBONE, classes=1, 
                     input_shape=(32, 128, 128, channels), 
                     encoder_weights='imagenet',
                     activation=activation,
-                    dropout=0.15
+                    dropout=args.dropout_rate
                     )
     model.compile(optimizer = optim, loss=total_loss, metrics=metrics)
 
-
-    initial_learning_rate = 0.001
     def lr_exp_decay(epoch, lr):
         k = 0.01
-        return initial_learning_rate * math.exp(-k*epoch)
+        return LR * math.exp(-k*epoch)
     early_stopping = EarlyStopping(monitor='val_loss', 
         patience=10, 
         min_delta=0, 
@@ -85,22 +72,23 @@ def main():
         min_lr=0.0001,
         verbose=2
     )
-    checkpoint_filepath = '/content/drive/MyDrive/Guard_cell_data/Models/'
-    filepath = 'my_best_model.epoch{epoch:02d}-loss{val_iou_score:.2f}.hdf5'
+    checkpoint_filepath = args.log_dir
+    filename = 'my_model.epoch{epoch:02d}-loss{val_iou_score:.2f}.hdf5'
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=os.path.join(checkpoint_filepath,filepath),
+        filepath=os.path.join(checkpoint_filepath,filename),
         save_weights_only=True,
         monitor='val_iou_score',
         mode='max',
         save_best_only=True,
         verbose = 1)
 
-    history=model.fit(X_train_prep, 
+    X_train, X_test, y_train, y_test = dataset_loader(args.img_dir, args.mask_dir)
+    model.fit(X_train, 
             y_train,
-            batch_size=8, 
-            epochs=100,
+            batch_size=args.batch_size, 
+            epochs=args.epochs,
             verbose=1,
-            validation_data=(X_test_prep, y_test),
+            validation_data=(X_test, y_test),
             callbacks=[LearningRateScheduler(lr_exp_decay, verbose=1), early_stopping, model_checkpoint_callback])
 
 

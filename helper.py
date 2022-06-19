9e150754c4f30dc509dc50de2f68fec5e58ed4df
Dolzodmaa Davaasuren <dolzodmaadavaasuren@Dolzodmaas-MacBook-Pro.local>
2022-06-19 16:25:16 -0700

    helper

--- helper.py
+++ helper.py
@@ -1,3 +1,119 @@
+SMOOTH = 1e-5
+class KerasObject:
+    _backend = None
+    _models = None
+    _layers = None
+    _utils = None
+
+    def __init__(self, name=None):
+        if (self.backend is None or
+                self.utils is None or
+                self.models is None or
+                self.layers is None):
+            raise RuntimeError('You cannot use `KerasObjects` with None submodules.')
+
+        self._name = name
+
+    @property
+    def __name__(self):
+        if self._name is None:
+            return self.__class__.__name__
+        return self._name
+
+    @property
+    def name(self):
+        return self.__name__
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @classmethod
+    def set_submodules(cls, backend, layers, models, utils):
+        cls._backend = backend
+        cls._layers = layers
+        cls._models = models
+        cls._utils = utils
+
+    @property
+    def submodules(self):
+        return {
+            'backend': self.backend,
+            'layers': self.layers,
+            'models': self.models,
+            'utils': self.utils,
+        }
+
+    @property
+    def backend(self):
+        return self._backend
+
+    @property
+    def layers(self):
+        return self._layers
+
+    @property
+    def models(self):
+        return self._models
+
+    @property
+    def utils(self):
+        return self._utils
+
+
+class Metric(KerasObject):
+    pass
+
+
+class Loss(KerasObject):
+
+    def __add__(self, other):
+        if isinstance(other, Loss):
+            return SumOfLosses(self, other)
+        else:
+            raise ValueError('Loss should be inherited from `Loss` class')
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __mul__(self, value):
+        if isinstance(value, (int, float)):
+            return MultipliedLoss(self, value)
+        else:
+            raise ValueError('Loss should be inherited from `BaseLoss` class')
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+
+class MultipliedLoss(Loss):
+
+    def __init__(self, loss, multiplier):
+
+        # resolve name
+        if len(loss.__name__.split('+')) > 1:
+            name = '{}({})'.format(multiplier, loss.__name__)
+        else:
+            name = '{}{}'.format(multiplier, loss.__name__)
+        super().__init__(name=name)
+        self.loss = loss
+        self.multiplier = multiplier
+
+    def __call__(self, gt, pr):
+        return self.multiplier * self.loss(gt, pr)
+
+
+class SumOfLosses(Loss):
+
+    def __init__(self, l1, l2):
+        name = '{}_plus_{}'.format(l1.__name__, l2.__name__)
+        super().__init__(name=name)
+        self.l1 = l1
+        self.l2 = l2
+
+    def __call__(self, gt, pr):
+        return self.l1(gt, pr) + self.l2(gt, pr)
+
 def _gather_channels(x, indexes, **kwargs):
     """Slice tensor along channels axis by given indexes"""
     backend = kwargs['backend']
@@ -52,8 +168,7 @@ def average(x, per_image=False, class_weights=None, **kwargs):
 # ----------------------------------------------------------------
 
 def iou_score(gt, pr, class_weights=1., class_indexes=None, smooth=SMOOTH, per_image=False, threshold=None, **kwargs):
-
-
+  
     backend = kwargs['backend']
 
     gt, pr = gather_channels(gt, pr, indexes=class_indexes, **kwargs)
@@ -72,7 +187,6 @@ def iou_score(gt, pr, class_weights=1., class_indexes=None, smooth=SMOOTH, per_i
 
 def f_score(gt, pr, beta=1, class_weights=1, class_indexes=None, smooth=SMOOTH, per_image=False, threshold=None,
             **kwargs):
-   
 
     backend = kwargs['backend']
 
@@ -111,7 +225,7 @@ def precision(gt, pr, class_weights=1, class_indexes=None, smooth=SMOOTH, per_im
 
 
 def recall(gt, pr, class_weights=1, class_indexes=None, smooth=SMOOTH, per_image=False, threshold=None, **kwargs):
-
+  
     backend = kwargs['backend']
 
     gt, pr = gather_channels(gt, pr, indexes=class_indexes, **kwargs)
@@ -154,8 +268,7 @@ def binary_crossentropy(gt, pr, **kwargs):
 
 
 def categorical_focal_loss(gt, pr, gamma=2.0, alpha=0.25, class_indexes=None, **kwargs):
-
-
+   
     backend = kwargs['backend']
     gt, pr = gather_channels(gt, pr, indexes=class_indexes, **kwargs)
 
@@ -169,7 +282,7 @@ def categorical_focal_loss(gt, pr, gamma=2.0, alpha=0.25, class_indexes=None, **
 
 
 def binary_focal_loss(gt, pr, gamma=2.0, alpha=0.25, **kwargs):
-
+ 
     backend = kwargs['backend']
 
     # clip to prevent NaN's and Inf's
--- loss.py
+++ loss.py
@@ -1,90 +1,7 @@
 
-import functional as F
-
+from helper import Loss, f_score,  binary_focal_loss
 SMOOTH = 1e-5
 
-class KerasObject:
-    _backend = None
-    _models = None
-    _layers = None
-    _utils = None
-
-    def __init__(self, name=None):
-        if (self.backend is None or
-                self.utils is None or
-                self.models is None or
-                self.layers is None):
-            raise RuntimeError('You cannot use `KerasObjects` with None submodules.')
-
-        self._name = name
-
-    @property
-    def __name__(self):
-        if self._name is None:
-            return self.__class__.__name__
-        return self._name
-
-    @property
-    def name(self):
-        return self.__name__
-
-    @name.setter
-    def name(self, name):
-        self._name = name
-
-    @classmethod
-    def set_submodules(cls, backend, layers, models, utils):
-        cls._backend = backend
-        cls._layers = layers
-        cls._models = models
-        cls._utils = utils
-
-    @property
-    def submodules(self):
-        return {
-            'backend': self.backend,
-            'layers': self.layers,
-            'models': self.models,
-            'utils': self.utils,
-        }
-
-    @property
-    def backend(self):
-        return self._backend
-
-    @property
-    def layers(self):
-        return self._layers
-
-    @property
-    def models(self):
-        return self._models
-
-    @property
-    def utils(self):
-        return self._utils
-
-
-
-class Loss(KerasObject):
-
-    def __add__(self, other):
-        if isinstance(other, Loss):
-            return SumOfLosses(self, other)
-        else:
-            raise ValueError('Loss should be inherited from `Loss` class')
-
-    def __radd__(self, other):
-        return self.__add__(other)
-
-    def __mul__(self, value):
-        if isinstance(value, (int, float)):
-            return MultipliedLoss(self, value)
-        else:
-            raise ValueError('Loss should be inherited from `BaseLoss` class')
-
-    def __rmul__(self, other):
-        return self.__mul__(other)
 
 class DiceLoss(Loss):
 
@@ -97,7 +14,7 @@ class DiceLoss(Loss):
         self.smooth = smooth
 
     def __call__(self, gt, pr):
-        return 1 - F.f_score(
+        return 1 - f_score(
             gt,
             pr,
             beta=self.beta,
@@ -118,7 +35,7 @@ class BinaryFocalLoss(Loss):
         self.gamma = gamma
 
     def __call__(self, gt, pr):
-        return F.binary_focal_loss(gt, pr, alpha=self.alpha, gamma=self.gamma, **self.submodules)
+        return binary_focal_loss(gt, pr, alpha=self.alpha, gamma=self.gamma, **self.submodules)
 
 
 dice_loss = DiceLoss()
